59e911bebfe35565a337f5d1dac11e78e103ea27
MarahShabello <s11819990@stu.najah.edu>
2023-05-23 12:34:10 +0300

    add last update

--- Crack_Detector/Crack_Detector.ino
+++ Crack_Detector/Crack_Detector.ino
@@ -3,7 +3,7 @@
 // Motor 1
 int MOTOR_1_PIN_1 = 2;
 int MOTOR_1_PIN_2 = 3;
-int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed`
+int speedPinA = 9; // Needs to be a PWM pin to be able to control motor speed
 
 // Motor 2
 int MOTOR_2_PIN_1 = 4;
@@ -11,7 +11,7 @@ int MOTOR_2_PIN_2 = 5;
 int speedPinB = 10; // Needs to be a PWM pin to be able to control motor speed
 
 unsigned long int value = 0; // stores the incoming hex value
-byte seq = 0; //stores the current number of executed sequences
+byte seq = 0; //stores the current number of executed sequenceso
 byte seq_Array[50];// array to store the movement sequence in terms of integers(1 for FWD, 2 for LEFT and so on..)
 
 //counter for counting the number of times program pass through a movement function(fwd, lft etc.)
@@ -37,12 +37,39 @@ unsigned long int total_Stp_Time[10];
 
 bool flag = false;
 
+#define RX 0
+#define TX 1
+
+int motorSpeed = 80;
+
+#include <SoftwareSerial.h>
+#include "string.h"
+SoftwareSerial SoftSerial(RX, TX);
+
+#include <SharpIR.h>
+
+// Define model and input pin:
+#define IRPin A0
+#define model 1080
+
+SharpIR mySensor = SharpIR(IRPin, model);
+
+#include <Servo.h>
+#define trigPin 7
+#define echoPin 8
+Servo servo;
+
 void setup() {  // Setup runs once per reset
   // initialize serial communication @ 9600 baud:
   Serial.begin(9600);
+  SoftSerial.begin(115200);
+
+  pinMode(trigPin, OUTPUT);
+  pinMode(echoPin, INPUT);
+  servo.attach(6);
+  servo.write(90);
 
   //Define L298N Dual H-Bridge Motor Controller Pins
-
   pinMode(MOTOR_1_PIN_1, OUTPUT);
   pinMode(MOTOR_1_PIN_2, OUTPUT);
   pinMode(speedPinA, OUTPUT);
@@ -53,43 +80,84 @@ void setup() {  // Setup runs once per reset
 
   flag = true;
 
-  goForward();
-  delay(7000);
-  goLeft(LOW, HIGH);
-  delay(3000);
-  goRight(LOW, HIGH);
-  delay(3000);
-  goBackward();
-  delay(7000);
-  goStop();
-  delay(1000);
+  //  goForward();
+  //  delay(7000);
+  //  goLeft(LOW, HIGH);
+  //  delay(3000);
+  //  goRight(LOW, HIGH);
+  //  delay(3000);
+  //  goBackward();
+  //  delay(7000);
+  //  goStop();
+  //  delay(1000);
 
-  goForward();
-  delay(7000);
-  goLeft(LOW, HIGH);
-  delay(3000);
-  goRight(LOW, HIGH);
-  delay(3000);
-  goBackward();
-  delay(7000);
-  goStop();
-  delay(7000);
+  //  goForward();
+  //  delay(7000);
+  //  goLeft(LOW, HIGH);
+  //  delay(3000);
+  //  goRight(LOW, HIGH);
+  //  delay(3000);
+  //  goBackward();
+  //  delay(7000);
+  //  goStop();
+  //  delay(7000);
 
   flag = false;
 }
 
 void loop() {
-  //  Serial.println("go_In_Seq");
   //  go_In_Seq();
-  Serial.println("go_Back_Seq");
-  go_Back_Seq();
+
+  String cmd;
+  SoftSerial.write("Hello Boss");
+  cmd = SoftSerial.readString() ;
+  cmd.trim();
+  Serial.print("cmd: ");
+  Serial.println(cmd);
+  cmd = "auto";
+
+  if (cmd == "Forward") {
+    goForward();
+  }
+  else if (cmd == "Right") {
+    goRight(LOW, HIGH);
+  }
+  else if (cmd == "Backward") {
+    goBackward();
+  }
+  else if (cmd == "Left") {
+    goLeft(LOW, HIGH);
+  }
+  else if (cmd == "Stop") {
+    goStop();
+  }
+  else if (cmd == "pathSaving") {
+    flag = true;
+  }
+  else if (cmd == "increase") {
+    increaseSpeed();
+  }
+  else if (cmd == "decrease") {
+    decreaseSpeed();
+  }
+  else if (cmd == "path") {
+    go_In_Seq();
+  }
+  else if (cmd == "auto") {
+    Serial.println("Auto Mode");
+    move_autoPath();
+  }
+  delay(500);
+
+  Serial.println("LED");
+  //  go_Back_Seq();
 }
 
 
 void goForward() {
-  analogWrite(speedPinA, 100);//Sets speed variable via PWM
-  analogWrite(speedPinB, 100);//Sets speed variable via PWM
-  //  Serial.println("goForward");
+  analogWrite(speedPinA, motorSpeed);//Sets speed variable via PWM
+  analogWrite(speedPinB, motorSpeed);//Sets speed variable via PWM
+
   if (flag) {
     Serial.println("Forward2");
     current_Time0 = millis();
@@ -164,8 +232,8 @@ void goRight(bool pin1, bool pin2) {
     seq++;
   }
 
-  analogWrite(speedPinA, 100);//Sets speed variable via PWM
-  analogWrite(speedPinB, 100);//Sets speed variable via PWM
+  analogWrite(speedPinA, motorSpeed);//Sets speed variable via PWM
+  analogWrite(speedPinB, motorSpeed);//Sets speed variable via PWM
 
   digitalWrite(MOTOR_1_PIN_1, pin2);
   digitalWrite(MOTOR_1_PIN_2, pin1);
@@ -210,8 +278,8 @@ void goLeft(bool pin1, bool pin2) {
     Serial.println(seq_Array[seq]);
     seq++;
   }
-  analogWrite(speedPinA, 100);//Sets speed variable via PWM
-  analogWrite(speedPinB, 100);//Sets speed variable via PWM
+  analogWrite(speedPinA, motorSpeed);//Sets speed variable via PWM
+  analogWrite(speedPinB, motorSpeed);//Sets speed variable via PWM
 
   digitalWrite(MOTOR_1_PIN_1, LOW);
   digitalWrite(MOTOR_1_PIN_2, LOW);
@@ -252,8 +320,8 @@ void goBackward() {
     Serial.println(seq_Array[seq]);
     seq++;
   }
-  analogWrite(speedPinA, 100);//Sets speed variable via PWM
-  analogWrite(speedPinB, 100);//Sets speed variable via PWM
+  analogWrite(speedPinA, motorSpeed);//Sets speed variable via PWM
+  analogWrite(speedPinB, motorSpeed);//Sets speed variable via PWM
 
   digitalWrite(MOTOR_1_PIN_1, LOW);
   digitalWrite(MOTOR_1_PIN_2, HIGH);
@@ -382,7 +450,6 @@ void go_Stop_Seq(int n) {
   delay(total_Stp_Time[n]);
 }
 
-//---------------
 void go_Back_Seq(void) {
   for (int i = seq; i >= 0; i--) {
     int value1 = 0;
@@ -435,4 +502,91 @@ void go_Back_Seq(void) {
         j = fwd_Counter; k = lft_Counter; l = rgt_Counter; m = bwd_Counter; n = stp_Counter;
     }
   }
+}
+
+void increaseSpeed () {
+  motorSpeed += 10;
+}
+
+void decreaseSpeed () {
+  motorSpeed -= 10;
+}
+
+void move_autoPath () {
+  long duration, distance;
+
+  digitalWrite(trigPin, LOW);
+  delayMicroseconds(2);
+  digitalWrite(trigPin, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin, LOW);
+
+  duration = pulseIn(echoPin, HIGH);
+  distance = (duration / 2) / 29.1;
+
+  if (distance < 10) {
+    Serial.print(distance);
+    Serial.println(" cm");
+    servo.write(0); //right
+    // servo.write(180); //left
+  }
+
+  else {
+    Serial.println("The distance is greater than 10cm");
+    // Serial.println("Forward");
+    servo.write(90);
+  }
+  // int distance_cm = mySensor.getDistance() / 2;
+
+  // // Print the measured distance to the serial monitor:
+  // Serial.print("Mean distance: ");
+  // Serial.print(distance_cm);
+  // Serial.println(" cm");
+
+  // if (distance_cm < 5) {
+  //   goStop();
+
+
+  //   // Rotate the sensor to the right
+  //   distance_cm = mySensor.getDistance() / 2;
+
+  //   // Print the measured distance to the serial monitor:
+  //   Serial.print("Mean distance inside {distance_cm < 5}: ");
+  //   Serial.print(distance_cm);
+  //   Serial.println(" cm");
+  //   if (distance_cm > 5) {
+  //     goRight(LOW, HIGH);
+  //   }
+  //   else {
+  //     // Rotate the sensor to the left
+  //     distance_cm = mySensor.getDistance() / 2;
+
+  //     // Print the measured distance to the serial monitor:
+  //     Serial.print("Mean distance...: ");
+  //     Serial.print(distance_cm);
+  //     Serial.println(" cm");
+  //     if (distance_cm > 5) {
+  //       goLeft(LOW, HIGH);
+  //     }
+  //   }
+  // }
+  // else {
+  //   goForward();
+  // }
+
+  //   if (distance_cm < 10) {
+  // Serial.print(distance_cm);
+  // Serial.println(" cm");
+  // servo.write(0); //right
+  // // servo.write(180); //left
+  // Serial.println("Distance is less than 10");
+  // }
+
+  // else {
+  // Serial.println("The distance is more than 10cm");
+  // // Serial.println("Forward");
+  // servo.write(90);
+  // }
+
+  delay(100);
 }
